6a5c180da9a15b21fca2a8241cbb0fb6f7948561
GAMGIN STUDIO <info@gamgin.net>
2026-05-16 21:42:19 +0200

    additional decision for solid rules-lang element position in rules-box (mean during scroll) and move scroll from box to main (don't know why I don't do it earlier)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { Game } from "./src/game.js";
 
-const admin = true;
+const admin = false;
 const easyStart = admin ? '1' : prompt('Do you want easy start without settings (2 players, max amount of cards - based on screen size, game time 20 min, turn time 10 sec, solid names) - enter: 1 for YES or 0 for NO');
 const isEasyStart = easyStart === '1' ? true : false;
 const game = await Game.init(isEasyStart);
